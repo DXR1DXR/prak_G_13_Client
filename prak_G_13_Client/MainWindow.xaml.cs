b81c082b1695fa3840a6150ce2c3c2d65d96981a
Alexei Kolobanov <tntbka123@gmail.com>
2023-01-24 19:41:38 +0300

    hgkhjgkjh

--- prak_G_13_Client/MainWindow.xaml.cs
+++ prak_G_13_Client/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text;
@@ -26,10 +28,25 @@ namespace prak_G_13_Client
         {
             Version latestVersion = Get_Latest_Version().Result;
             var currentversion = Assembly.GetExecutingAssembly().GetName().Version;
-            if (currentversion < latestVersion)
+            if (currentversion == latestVersion)//TODO <
             {
                 var tempPath = System.IO.Path.GetTempPath();
                 var tempProjPath = tempPath + "\\prak_G_13";
+                var updaterPath = tempProjPath + "\\Updater.exe";
+                Directory.CreateDirectory(tempProjPath);
+                var ress = GetType().Assembly.GetManifestResourceNames();
+                using (Stream resource = new MemoryStream(Properties.Resources.Updater))
+                {
+                    if (resource == null)
+                    {
+                        throw new ArgumentException("No such resource", "resourceName");
+                    }
+                    using (Stream output = File.OpenWrite(updaterPath))
+                    {
+                        resource.CopyTo(output);
+                    }
+                }
+                Process.Start(updaterPath, new List<string>() { Directory.GetCurrentDirectory() + "\\prak_G_13_Client.exe" });
             }
             InitializeComponent();
         }
