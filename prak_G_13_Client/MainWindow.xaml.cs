6b7a996511b0cdc7fff680e159a684d70164b508
Alexei Kolobanov <tntbka123@gmail.com>
2023-01-24 18:25:42 +0300

    hcghgxfvchgfc

--- prak_G_13_Client/MainWindow.xaml.cs
+++ prak_G_13_Client/MainWindow.xaml.cs
@@ -26,6 +26,10 @@ namespace prak_G_13_Client
         {
             Version latestVersion = Get_Latest_Version().Result;
             var currentversion = Assembly.GetExecutingAssembly().GetName().Version;
+            if (currentversion < latestVersion)
+            {
+                //Logic
+            }
             InitializeComponent();
         }
         private async Task<Version> Get_Latest_Version()
@@ -33,7 +37,6 @@ namespace prak_G_13_Client
             GitHubClient gitHubClient = new GitHubClient(new ProductHeaderValue("prak_G_13_Client"));
             var releases = gitHubClient.Repository.Release.GetLatest("DXR1DXR", "prak_G_13_Client");
             var latestVersion = new Version(releases.Result.Name.Remove(0, 1));
-
             return latestVersion;
         }
         
