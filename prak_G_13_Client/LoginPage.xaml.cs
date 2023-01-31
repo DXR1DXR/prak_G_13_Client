492481482b7ed68b8037f2b96dae698ee89015f6
Alexei Kolobanov <tntbka123@gmail.com>
2023-01-31 06:31:46 +0300

    тиллплплполопа

--- prak_G_13_Client/LoginPage.xaml.cs
+++ prak_G_13_Client/LoginPage.xaml.cs
@@ -1,18 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using prak_G_13_Client.Properties;
+using System;
 using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
+using VkNet;
+using VkNet.Enums.Filters;
+using VkNet.Model;
 
 namespace prak_G_13_Client
 {
@@ -21,6 +15,10 @@ namespace prak_G_13_Client
     /// </summary>
     public partial class LoginPage : Page
     {
+        public LoginPage(int vkID)
+        {
+            InitializeComponent();
+        }
         public LoginPage()
         {
             InitializeComponent();
@@ -39,6 +37,30 @@ namespace prak_G_13_Client
                 AppInfo.GetInstance().userId = Convert.ToInt32(userId);
                 NavigationService.Navigate(new StuffPage());
             }
+        }
+
+        private void EnterVKBT_Click(object sender, RoutedEventArgs e)
+        {
+            VkApi api = new VkApi();
+            var login = LoginTB.Text;
+            var password = PasswordPB.Password;
+            try
+            {
+                api.Authorize(new ApiAuthParams() { Login = login, Password = password, ApplicationId = 51539239 });
+            }
+            catch (Exception ex)
+            {
+                using (var client = new HttpClient())
+                {
+                    client.DefaultRequestHeaders.Accept.Clear();
+                    client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("text/html"));
+                    ulong appId = 51539239;
+                    api.Authorize(new ApiAuthParams() { ApplicationId = appId, Login = login, Password = password, Settings = VkNet.Enums.Filters.Settings.All, TwoFactorAuthorization = () => { string code = "454526"; return code; } });
+                    MessageBox.Show(ex.Message);
+                }
+                
+            }
+
         }
     }
 }
