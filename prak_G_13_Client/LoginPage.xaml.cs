707896b4f794c2829dc6404dfe7484d1a745fe26
Alexei Kolobanov <tntbka123@gmail.com>
2023-02-01 11:49:39 +0300

    tydrtydtxyxtd

--- prak_G_13_Client/LoginPage.xaml.cs
+++ prak_G_13_Client/LoginPage.xaml.cs
@@ -18,13 +18,10 @@ namespace prak_G_13_Client
     /// </summary>
     public partial class LoginPage : Page
     {
-        public LoginPage(int vkID)
-        {
-            InitializeComponent();
-        }
         public LoginPage()
         {
             InitializeComponent();
+            Toggle.Checked += Toggle_Checked;
         }
 
         private void EnterBT_Click(object sender, RoutedEventArgs e)
@@ -33,38 +30,72 @@ namespace prak_G_13_Client
             client.BaseAddress = new Uri(AppInfo.GetInstance().baseAdress);
             client.DefaultRequestHeaders.Accept.Clear();
             client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
-            HttpResponseMessage responseMessage = client.GetAsync($"Users/{LoginTB.Text}/{PasswordPB.Password}/{1}").Result;
-            string userId = responseMessage.Content.ReadAsStringAsync().Result;
-            if (userId != "null")
+            if ((bool)Toggle.IsChecked)
             {
-                AppInfo.GetInstance().userId = Convert.ToInt32(userId);
-                NavigationService.Navigate(new StuffPage());
+                HttpResponseMessage responseMessage = client.GetAsync($"Users/{LoginTB.Text}/{PasswordPB.Password}/{1}").Result;
+                string userId = responseMessage.Content.ReadAsStringAsync().Result;
+                if (userId != "false")
+                {
+                    AppInfo.GetInstance().userId = Convert.ToInt32(userId);
+                    MessageBox.Show("Успешная авторизация!");
+                    NavigationService.Navigate(new StuffPage());
+                }
+                else
+                {
+                    MessageBox.Show("Неверный логин или пароль");
+                }
             }
+            else
+            {
+                HttpResponseMessage responseMessage = client.GetAsync($"Users/{LoginTB.Text}").Result;
+                string userId = responseMessage.Content.ReadAsStringAsync().Result;
+                if (userId == "false")
+                {
+                    HttpResponseMessage responseMessage1 = client.PostAsync($"Users/{LoginTB.Text}/{PasswordPB.Password}/0", new StringContent("")).Result;
+                    MessageBox.Show("Успешная регистрация!");
+                }
+                else
+                {
+                    MessageBox.Show("Пользователь с таким логином уже существует");
+                }
+            }
+            
         }
 
         private void EnterVKBT_Click(object sender, RoutedEventArgs e)
         {
-            VkApi api = new VkApi();
-            var login = LoginTB.Text;
-            var password = PasswordPB.Password;
-            ulong appId = 51539239;
-            try
-            {
-                api.Authorize(new ApiAuthParams { Login = login, Password = password, Settings = VkNet.Enums.Filters.Settings.All });
-            }
-            catch (Exception ex)
-            {
-                if (ex.Message.Contains("Неправильный"))
-                {
-                    //...
-                } else if(ex.Message.Contains("Two")){
-                    api = new VkApi();
+            //VkApi api = new VkApi();
+            //var login = LoginTB.Text;
+            //var password = PasswordPB.Password;
+            //ulong appId = 51539239;
+            //try
+            //{
+            //    api.Authorize(new ApiAuthParams { Login = login, Password = password, Settings = VkNet.Enums.Filters.Settings.All });
+            //}
+            //catch (Exception ex)
+            //{
+            //    if (ex.Message.Contains("Неправильный"))
+            //    {
+            //        //...
+            //    } else if(ex.Message.Contains("Two")){
+            //        api = new VkApi();
 
-                    api.Authorize(new ApiAuthParams { Login = login, Password = password, Settings = VkNet.Enums.Filters.Settings.All });
-                }
+            //        api.Authorize(new ApiAuthParams { Login = login, Password = password, Settings = VkNet.Enums.Filters.Settings.All });
+            //    }
 
-            }
+            //}
+            NavigationService.Navigate(new TwoFactor());
 
+        }
+
+        private void Toggle_Checked(object sender, RoutedEventArgs e)
+        {
+            EnterBT.Content = "Войти";
+        }
+
+        private void Toggle_Unchecked(object sender, RoutedEventArgs e)
+        {
+            EnterBT.Content = "Регистрироваться";
         }
     }
 }
--- prak_G_13_Client/TwoFactor.xaml.cs
+++ prak_G_13_Client/TwoFactor.xaml.cs
@@ -11,17 +11,57 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using VkNet;
+using VkNet.Model;
 
 namespace prak_G_13_Client
 {
     /// <summary>
     /// Логика взаимодействия для TwoFactor.xaml
     /// </summary>
-    public partial class TwoFactor : Window
+    public partial class TwoFactor : Page
     {
         public TwoFactor()
         {
             InitializeComponent();
+        }
+
+        private void EnterBT_Click(object sender, RoutedEventArgs e)
+        {
+            VkApi api = new VkApi();
+            var login = LoginTB.Text;
+            var password = PasswordPB.Password;
+            ulong appId = 51539239;
+            try
+            {
+                api.Authorize(new ApiAuthParams { ApplicationId = appId, Login = login, Password = password, Settings = VkNet.Enums.Filters.Settings.All });
+                
+            }
+            catch (Exception ex)
+            {
+                if (ex.Message.Contains("Неверный"))
+                {
+                    MessageBox.Show("Неправильный логин или пароль");
+                }
+                else if (ex.Message.Contains("Двух"))
+                {
+                    if (CodeTB.Text == "")
+                    {
+                        MessageBox.Show("Необходим код 2FA");
+                        return;
+                    }
+                    api = new VkApi();
+                    string code = CodeTB.Text;
+                    api.Authorize(new ApiAuthParams { ApplicationId = appId, Login = login, Password = password, Settings = VkNet.Enums.Filters.Settings.All, TwoFactorAuthorization = () => { return code; } });
+                    if (api.UserId == null)
+                    {
+                        MessageBox.Show("Код 2FA неверен");
+                        return;
+                    }
+                    MessageBox.Show("Успешная авторизация!");
+                }
+
+            }
         }
     }
 }
